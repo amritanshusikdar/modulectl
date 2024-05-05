350d49ee1daeb2e27f8f2663dba480f489e18164
Amritanshu Sikdar <amritanshu.sikdar@gmail.com>
2024-05-05 15:48:04 +0200

    add contentprovider for defaultcr

---
+++ internal/scaffold/contentprovider/defaultcr.go
@@ -0,0 +1,18 @@
+package contentprovider
+
+type DefaultCRContentProvider struct{}
+
+func NewDefaultCRContentProvider() *DefaultCRContentProvider {
+	return &DefaultCRContentProvider{}
+}
+
+func (s *DefaultCRContentProvider) GetDefaultContent() string {
+	return s.getDefaultContent()
+}
+
+func (s *DefaultCRContentProvider) getDefaultContent() string {
+	return `# This is the file that contains the defaultCR for your module, which is the Custom Resource that will be created upon module enablement.
+	# Make sure this file contains *ONLY* the Custom Resource (not the Custom Resource Definition, which should be a part of your module manifest)
+
+`
+}
